8dc6db7b4067d065f32f13140efbe238f01d6572
Khun Zohn <khunzohn@gmail.com>
2020-12-06 21:47:22 +0630

    finish exe 3.12

--- mongo.js
+++ mongo.js
@@ -1,31 +1,43 @@
 const mongoose = require('mongoose')
 
 if(process.argv.length < 3) {
-    console.log('Pliease provide the password as an argument: node mongo.js <password>')
+    console.log('Pliease provide the password, name, number as arguments: node mongo.js <password> <name> <number>')
     process.exit(1)
 }
 
 const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
 
-const url = `mongodb+srv://read-write:%{password}@sandbox.jnf1d.mongodb.net/note-app?retryWrites=true&w=majority`
+const url = `mongodb+srv://admin:${password}@sandbox.jnf1d.mongodb.net/phone-app?retryWrites=true&w=majority`
 
-mongoose.connect(url, { userNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true, useCreateIndex: true})
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true, useCreateIndex: true})
 
-const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
-    important: Boolean,
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
 })
 
-const Note = mongoose.model('Note', noteSchema)
+const Person = mongoose.model('Person', personSchema)
 
-const note = Note({
-    content: 'HTML is easy',
-    date: new Date(),
-    important: true,
-})
+if(!name || !number) {
+    Person.find({}).then(result => {
+        console.log("phonebook:")
+        result.forEach(person => {
+            console.log(person.name, person.number)
+        })
 
-note.save().then(result => {
-    console.log('Note saved!')
     mongoose.connection.close()
-})
+    })
+
+} else {
+    const person = Person({
+        name: name,
+        number: number
+    })
+    
+    person.save().then(result => {
+        console.log('added',name,"number",number, "to phonebook")
+        mongoose.connection.close()
+    })
+}
